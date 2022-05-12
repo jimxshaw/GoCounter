fb5548d8dd6fc159466b2f97633e013858f21624
Jim Shaw <jimxshaw@gmail.com>
2022-05-11 22:02:56 -0500

    Add -b flag to count bytes.

--- main.go
+++ main.go
@@ -13,29 +13,33 @@ func main() {
 	// The default value is false, meaning count words by default.
 	lines := flag.Bool("l", false, "Count lines")
 
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parse the user provided flag.
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// This is used to read text from a Reader, such as files.
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else if !countLines {
 		// If the count lines flag is not set then we count by words.
 		// Define the split type, which is words. The default is by lines.
 		scanner.Split(bufio.ScanWords)
 	}
 
 	// Define a counter.
-	wordCounter := 0
+	counter := 0
 
 	// Increment the counter for every word or line scanned.
 	for scanner.Scan() {
-		wordCounter++
+		counter++
 	}
 
-	return wordCounter
+	return counter
 }
