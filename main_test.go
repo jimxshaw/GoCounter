008cf3a845f67bd58e9daba7a54866c40354ecb0
Jim Shaw <jimxshaw@gmail.com>
2022-05-11 22:10:08 -0500

    Add TestCountBytes.

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ func TestCountWords(t *testing.T) {
 	testInput := bytes.NewBufferString("one two three four five six\n")
 
 	expected := 6
-	got := count(testInput, false)
+	got := count(testInput, false, false)
 
 	if got != expected {
 		t.Errorf("Expected %d, got %d instead.\n", expected, got)
@@ -20,7 +20,18 @@ func TestCountLines(t *testing.T) {
 	testInput := bytes.NewBufferString("This is Line 1.\nThis is Line 2.\nThis is Line 3.")
 
 	expected := 3
-	got := count(testInput, true)
+	got := count(testInput, true, false)
+
+	if got != expected {
+		t.Errorf("Expected %d, got %d instead.\n", expected, got)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	testInput := bytes.NewBufferString("Curious how many bytes this string will output.")
+
+	expected := 47
+	got := count(testInput, false, true)
 
 	if got != expected {
 		t.Errorf("Expected %d, got %d instead.\n", expected, got)
